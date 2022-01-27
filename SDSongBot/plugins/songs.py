99851871665ee675614bb849933826e3fbebae82
MrityunjayX <94744378+MrityunjayX@users.noreply.github.com>
2022-01-27 14:42:53 +0530

    Update songs.py

--- SDSongBot/plugins/songs.py
+++ SDSongBot/plugins/songs.py
@@ -1,6 +1,6 @@
 # Plugin by @Mr_Dark_Prince
 #SDBOTs <https://t.me/SDBOTs_Inifinity>
-
+import wget
 import os
 import requests
 import aiohttp
@@ -70,3 +70,74 @@ def song(client, message):
         os.remove(thumb_name)
     except Exception as e:
         print(e)
+        
+        
+        
+ def get_arg(message):
+
+    msg = message.text
+
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+
+    split = msg[1:].replace("\n", " \n").split(" ")
+
+    if " ".join(split[1:]).strip() == "":
+
+        return ""
+
+    return " ".join(split[1:])
+
+
+
+@app.on_message(filters.command("song", "!"))
+
+async def song(client, message):
+
+    message.chat.id
+
+    message.from_user["id"]
+
+    args = get_arg(message) + " " + "song"
+
+    if args.startswith(" "):
+
+        await message.reply("<b>Enter song name❗</b>")
+
+        return ""
+
+    m = await message.reply_text(
+
+        "Downloading your song,\nPlz wait ⏳️"
+
+    )
+
+    try:
+
+        r = requests.get(f"https://jostapi.herokuapp.com/saavn?query={args}")
+
+    except Exception as e:
+
+        await m.edit(str(e))
+
+        return
+
+    sname = r.json()[0]["song"]
+
+    slink = r.json()[0]["media_url"]
+
+    ssingers = r.json()[0]["singers"]
+
+    file = wget.download(slink)
+
+    ffile = file.replace("mp4", "m4a")
+
+    os.rename(file, ffile)
+
+    await message.reply_audio(audio=ffile, title=sname, performer=ssingers)
+
+    os.remove(ffile)
+
+    await m.delete()
+
+       
+
